66cf42d1bc0ff1ad684d9c07764f1b3a7fc20b6d
Moésio Filho <moesio.f@gmail.com>
2025-08-06 21:01:45 -0300

    refactor(market-scrapers): update compose and script

--- market-scrapers/scripts/run_blocking.sh
+++ market-scrapers/scripts/run_blocking.sh
@@ -8,13 +8,13 @@ while true
 do
     echo "[run_blocking] Starting execution at $(date +%T)."
 
-    # Run price scraper
-    echo "[run_blocking] Running Market Price scraper."
-    python -m scrapers.market_price -f
-
     # Run earning scraper
     echo "[run_blocking] Running Published Earnings scraper."
     python -m scrapers.published_earnings
+
+    # Run price scraper
+    echo "[run_blocking] Running Market Price scraper."
+    python -m scrapers.market_price -f
 
     # Sleep for specified time
     sleep ${SLEEP_INTERVAL:-0.5d}
